8b4bed48f1e14752a85d6d92d4d71c50efa31d87
janani.it19 <janani.it19@bitsathy.ac.in>
2019-11-09 15:55:28 +0530

    first commit

---
+++ prog67.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("Enter 2 values:");
+scanf("%d\t%d",&a,&b);
+if(a>b)
+printf("The grearest number is %d",a);
+else
+printf("The greatest number is %d",b);
+}
